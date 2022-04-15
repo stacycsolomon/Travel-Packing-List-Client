9d7d2e91da6c5e8f3eb80582c6cca4cb42596102
Stacy S <stacycsolomon@gmail.com>
2022-04-15 16:58:37 -0400

    Updated trip.ui
    Fixed show/fadeout for display and error messages to allow them to be visible again once a fade out function completes

--- app/trip/ui.js
+++ app/trip/ui.js
@@ -7,15 +7,15 @@ const onIndexTripsSuccess = function (response) {
                     <br>
                      <p>${trip.destination}</p>
                         <p>${trip.date}</p>
-                        <p>${trip.length}</p>
+                        <p>${trip.length} nights</p>
             
       <form class="update-trip" id="update-${trip._id}" data-id=${trip._id}>
-          <input type="text" name="trip[destination]" placeholder="Destination" required>
-          <input type="date" name="trip[date]" placeholder="Date of Trip" required>
-          <input type="text" name="trip[length]" placeholder="Length of Stay" required>
+          <input type="text" name="trip[destination]" placeholder="Destination">
+          <input type="date" name="trip[date]" placeholder="Date of Trip">
+          <input type="number" name="trip[length]" placeholder="Length of Stay">
           <button type="submit">Update Trip</button>
        </form><br>
-      <button class="delete-trip" data-id=${trip._id}>Delete trip</button>
+      <button class="delete-trip" data-id=${trip._id}>Delete</button>
       <button class="edit-trip" data-id=${trip._id}>Edit</button>
   
                       </div>  
@@ -27,7 +27,8 @@ const onIndexTripsSuccess = function (response) {
   $('#error-message').text('')
 }
 const onIndexTripsFailure = function () {
-  $('#error-message').text('There was an error').fadeOut(3000)
+  $('#error-message').show()
+  $('#error-message').text('There was an error').fadeOut(5000)
 }
 
 const onFindTripSuccess = function (response) {
@@ -45,7 +46,8 @@ const onFindTripSuccess = function (response) {
 }
 
 const onFindTripFailure = function () {
-  $('#error-message').text('Failure while trying to find trip').fadeOut(3000)
+  $('#error-message').show()
+  $('#error-message').text('Failure while trying to find trip').fadeOut(5000)
 }
 
 const onAddTripSuccess = function (response) {
@@ -53,38 +55,45 @@ const onAddTripSuccess = function (response) {
                       <div>
                         <p>${response.trip.destination}</p>
                         <p>${response.trip.date}</p>
-                        <p>${response.trip.length}</p> 
+                        <p>${response.trip.length} nights</p> 
+                        
                       </div>
                     `
   $('#trips-display').html(tripHtml)
-  $('#display').text('Success trip added').fadeOut(3000)
+  $('#display').show()
+  $('#display').text('Success trip added').fadeOut(5000)
   $('form').trigger('reset')
   $('#error-message').text('')
   console.log(response)
 }
 
 const onAddTripFailure = function () {
-  $('#error-message').text('Failure while adding trip').fadeOut(3000)
+  $('#error-message').show()
+  $('#error-message').text('Failure while adding trip').fadeOut(5000)
 }
 
 const onUpdateTripSuccess = function (id) {
   console.log(id)
   console.log($(`#${id}`))
-  $('#display').text('Trip successfully updated').fadeOut(3000)
+  $('#display').show()
+  $('#display').text('Trip successfully updated').fadeOut(5000)
   $('form').trigger('reset')
 }
 
 const onUpdateTripFailure = function () {
-  $('#error-message').text('Failure while trying to update trip').fadeOut(3000)
+  $('#error-message').show()
+  $('#error-message').text('Failure while trying to update trip').fadeOut(5000)
 }
 
 const onDeleteTripSuccess = function () {
-  $('#display').text('Success. Trip deleted!').fadeOut(3000)
+  $('#display').show()
+  $('#display').text('Success. Trip deleted!').fadeOut(5000)
   $('form').trigger('reset')
 }
 
 const onDeleteTripFailure = function () {
-  $('#error-message').text('Failure while trying to delete trip').fadeOut(3000)
+  $('#error-message').show()
+  $('#error-message').text('Failure while trying to delete trip').fadeOut(5000)
 }
 
 module.exports = {
@@ -99,5 +108,3 @@ module.exports = {
   onDeleteTripSuccess,
   onDeleteTripFailure
 }
-
-//  <div id=${trip._id}></div>
