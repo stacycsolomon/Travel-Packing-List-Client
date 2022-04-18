4c9b20f3d761c82d269a2c6a3c472fc9f647a776
Stacy S <stacycsolomon@gmail.com>
2022-04-17 23:56:11 -0400

    Updated curl-scripts

--- curl-scripts/trips/update.sh
+++ curl-scripts/trips/update.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-TOKEN="8de10008ca0f240f88177a0aa8857707"
-ID="6255d496d1cb1b490a307c97"
-DESTINATION="Kingston"
-DATE="03/03/2022"
-LENGTHOFSTAY="2"
-
 API="http://localhost:4741"
 URL_PATH="/trips"
 
